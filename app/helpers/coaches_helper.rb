f1657c9675d858df885cd0490c4fa0bb65474d3b
Diego <diegoyon@outlook.com>
2023-01-06 15:14:40 -0600

    display available times that coaches have

--- app/helpers/coaches_helper.rb
+++ app/helpers/coaches_helper.rb
@@ -1,2 +1,18 @@
 module CoachesHelper
+  def day_times(day, appointments)
+    # appointments.each do |appointment| 
+    #   # if appointment.day == "Monday"
+    #     appointment.time 
+    #   # end 
+    # end
+    res = []
+
+    appointments.each do |appointment|
+      if appointment.day == day
+        res << appointment.time 
+      end 
+    end
+
+    res
+  end
 end
